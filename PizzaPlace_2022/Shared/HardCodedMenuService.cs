bdd910ec2e4ee98d00cc1ef8ea15a4243dd91313
xuantruong23 <19K4081033@hce.edu.vn>
2022-06-26 23:55:24 +0700

    first commit

---
+++ PizzaPlace_2022/Shared/HardCodedMenuService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace_2022.Shared {
+    public class HardCodedMenuService : IMenuService {
+        public ValueTask<Menu> GetMenu() => new ValueTask<Menu>(
+            new Menu {
+                Pizzas = new List<Pizza> {
+                    //new Pizza(1, "Pepperoni", 8.99M, Spiciness.Spicy),
+                    //new Pizza(2, "Margarita", 7.99M, Spiciness.None),
+                    //new Pizza(3, "Diabolo", 9.99M, Spiciness.Hot)
+                    new Pizza(4, "Xe sirius", 35M, Spiciness.xe1),
+                    new Pizza(5, "Xe vision", 47M, Spiciness.xe2),
+                    new Pizza(6, "Xe SH mode", 60M, Spiciness.xe3),
+                    new Pizza(7, "Xe Grande", 55M, Spiciness.xe4),
+                    new Pizza(8, "Xe Airblade", 48M, Spiciness.xe5),
+                }
+             });
+    }
+}
+
---
+++ PizzaPlace_2022/Shared/Spiciness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace_2022.Shared {
+    public enum Spiciness {
+        xe1,
+        xe2,
+        xe3,
+        xe4,
+        xe5,
+    }
+}
